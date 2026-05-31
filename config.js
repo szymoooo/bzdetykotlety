06c17aed373703f84339f4c2553693c217e0dbcc
szymoooo <szymonbaj@gmail.com>
2026-05-31 09:32:59 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - TRZYMAJ TYLKO LOKALNIE, NIE WRZUCAJ NA GITHUB!
+const SUPABASE_CONFIG = {
+    url: 'https://fhxemxbrnihvazqybfir.supabase.co/rest/v1/',
+    anonKey: 'sb_publishable_kinMW3A9VlWRmaJbph77WA_ov5A7qeJ'
+};
